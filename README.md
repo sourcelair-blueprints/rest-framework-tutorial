12b7dc0e9f6de662e55afe351c94fbb95edb2da2
Paris <paris@sourcelair.com>
2015-09-18 13:45:50 +0300

    Improve onboarding experience
    - Add Blueprint link to README.md
    - Auto `pip install` and auto `manage.py migrate` on repo initialization

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Source code for the [Django REST framework tutorial][tut].
 
+Start coding now on the REST framework tutorial at [SourceLair](https://www.sourcelair.com/blueprints/sourcelair/rest-framework-tutorial).
+
 [tut]: http://tomchristie.github.com/django-rest-framework/tutorial/1-serialization
--- manage.py
+++ manage.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 import os
+import subprocess
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
+    # HACK: Install dependencies if not already installed
+    PIP_LOG = '.pip.log'
+    if not os.path.exists(PIP_LOG):
+        with open(PIP_LOG, 'wb') as LOG_FILE:
+            print 'Installing dependencies...'
+            subprocess.check_call(
+                ['pip', 'install', '-r', '../requirements.txt'],
+                stdout=LOG_FILE,
+                stderr=LOG_FILE
+            )
+            print 'Dependencies installed...'
+            # We have to fork this process in order to get the new installed
+            # packages.
+            # HACK: Migrate before running process since this is the fist
+            # time the app runs
+            subprocess.call(['python', 'manage.py', 'migrate'])
+            subprocess.call(['python'] + sys.argv)
+    else:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "braintree_api.settings"
+        )
 
-    from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+        execute_from_command_line(sys.argv)
